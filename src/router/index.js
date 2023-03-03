f8adeb59e1f55bf4f013be642b29b205e2631b21
Feuvrier-Eliot <95072076+Feuvrier-Eliot@users.noreply.github.com>
2023-03-03 17:15:10 +0100

    mise en place des dossiers

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue';
+import View404 from '../views/View404.vue';
+import ProjectsView from '../views/projets/ListeView.vue';
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    { path: '/', name: 'Home', component: HomeView },
+    { path: '/admin', name: 'AdminView', component: AdminView },
+    { path: '/projects', name: 'ProjectsView', component: ProjectsView },
+
+    { path: '/:pathMatch(.*)*', name: 'View404', component: View404 },
+
+
+
   ]
 })
+
 
 export default router
