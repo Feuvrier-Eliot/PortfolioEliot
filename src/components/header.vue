b2268691e5fb779f02483fa790ad6bd5549481c6
Feuvrier-Eliot <95072076+Feuvrier-Eliot@users.noreply.github.com>
2023-03-09 23:45:31 +0100

    background animé

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class=" bg-red-600  sticky top-0 drop-shadow-lg z-10 ">
+    <header class="sticky top-0 drop-shadow-lg z-10 ">
         <a href="#content" class="sr-only focus:not-sr-only text-lg text-white"> Passez au contenu </a>
         <nav class=" lg:flex lg:justify-between items-center">
           <div class="flex justify-between items-center">
@@ -28,18 +28,13 @@
             <li class="my-5">
               <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm text-white font-museomoderno " to="/projects">Projects</RouterLink>
             </li>
-            <li class="my-5">
-              <RouterLink v-if="isAdmin" class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm text-white font-museomoderno " to="/profil">Mon Profil</RouterLink>
-            </li>
+
             <li class="my-5">
               <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm text-white font-museomoderno " to="/about">About Me</RouterLink>
             </li>
             <li class="my-5">
               <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-white" to="/contact">Contact</RouterLink>
             </li>
-            <li class="my-5">
-          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-white " to="/admin">Connexion</RouterLink>
-        </li>
           </ul>
           </Transition>
           <ul class="lg:flex  gap-4 lg:items-center text-white text-xl font-algerian mx-6 py-4 hidden" >
@@ -55,12 +50,8 @@
             <li class="my-5">
               <RouterLink class="border-2 border-white my-6 lg:px-2 xl:pt-2 rounded-2xl font-museomoderno text-white " to="/contact">Contact</RouterLink>
             </li>
-            <li class="my-5">
-              <RouterLink v-if="isAdmin" class="border-2 border-white my-6 lg:px-2 xl:pt-2 rounded-2xl font-museomoderno text-white" to="/profil">Mon Profil</RouterLink>
-            </li>
-            <li class="my-5">
-          <RouterLink class="my-6 lg:px-4 xl:pt-2 xl:rounded-sm font-museomoderno text-white " to="/admin">Connexion</RouterLink>
-        </li>
+
+
           </ul>    
         </nav>
       </header>
@@ -71,24 +62,6 @@
     <script>
     import Navigation from './icones/Navbar.vue'
     import { MenuIcon, } from "@heroicons/vue/solid";
-    
-    import {
-      getFirestore,
-      collection,
-      onSnapshot,
-      query,
-      where
-    } from 'https://www.gstatic.com/firebasejs/9.17.0/firebase-firestore.js'
-    import {
-      getStorage,
-      ref,
-      getDownloadURL,
-    } from 'https://www.gstatic.com/firebasejs/9.17.0/firebase-storage.js'
-    
-    import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.0/firebase-auth.js'
-    
-    // Import emetteur de main.js
-    import { emitter } from '../main.js'
     
     export default {
       components:{ Navigation, MenuIcon },
@@ -105,78 +78,5 @@
           isAdmin:false       // Si l'utilisateur est ou non administrateur
         }
       },
-      mounted(){
-        // Vérifier si un user connecté existe déjà
-        // Au lancement de l'application
-        this.getUserConnect();
-    
-        // Ecoute de l'évènement de connexion
-        emitter.on('connectUser', e => {
-          // Récupération du user
-          this.user = e.user;
-    //      console.log('App => Reception user connecté', this.user);
-          // Recherche infos complémentaires du user
-          this.getUserInfo(this.user);      
-        })        
-        
-        // Ecoute de l'évènement de deconnexion
-        emitter.on('deConnectUser', e => {
-          // Récupération user 
-          this.user = e.user;
-    //      console.log('App => Reception user deconnecté', this.user);
-          // Réinitialisation infos complémentaires user
-          this.userInfo   = null;
-          this.name       = 'Vidéo';
-          this.isAdmin    = false;
-        })
-      },
-      methods:{
-        // Obtenir les informations du user connecté
-        async getUserConnect(){
-          await getAuth().onAuthStateChanged(function(user) { 
-            if(user) {
-                // Récupération du user connecté
-                this.user = user;
-                // Recherche de ses infos complémentaires
-                this.getUserInfo(this.user);   
-            } 
-          }.bind(this));
-          // Noter le bind(this), cas des zones isolées
-          // lors de 2 strucutres imbiquées, Vue perd la visibilité
-          // des données
-          // On les ré injecte par le mot-clef this
-        },
-    
-        async getUserInfo(user){
-          // Rechercher les informations complémentaires de l'utilisateur
-          // Obtenir Firestore
-          const firestore = getFirestore();
-          // Base de données (collection)  document participant
-          const dbUsers = collection(firestore, "users");
-          // Recherche du user par son uid
-          const q = query(dbUsers, where("uid", "==", user.uid));
-          await onSnapshot(q, (snapshot) => {
-              this.userInfo = snapshot.docs.map(doc => (
-                {id:doc.id, ...doc.data()}
-              ));
-    //          console.log("userInfo", this.userInfo);                
-              // userInfo étant un tableau, onn récupère
-              // ses informations dans la 1° cellule du tableau : 0
-              this.name = this.userInfo[0].login;
-              this.isAdmin=this.userInfo[0].admin;
-              // Recherche de l'image du user sur le Storage
-              const storage = getStorage();
-              // Référence du fichier avec son nom
-              const spaceRef = ref(storage, 'users/'+this.userInfo[0].avatar);
-              getDownloadURL(spaceRef)
-                .then((url) => {
-                  this.avatar = url;
-                })
-                .catch((error) =>{
-                  console.log('erreur downloadUrl', error);
-                })
-            });
-          }
-        } 
     }
     </script>
